42c69d8672f1ed0fcf429323d65d5b75c3f332d5
Jackson-Wozniak <jjw30@comcast.net>
2023-05-19 15:37:50 -0400

    styling changes to spending categories

--- frontend/src/components/SpendingCategory.jsx
+++ frontend/src/components/SpendingCategory.jsx
@@ -1,11 +1,28 @@
+import '../styles/SpendingCategory.css';
+
 function SpendingCategory(props) {
 
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+
     let category = props.category;
+    let sum = category.goal - category.sumOfExpenses;
+    let formattedSum = formatter.format(sum);
+    let totalToSpend = (category.goal - category.sumOfExpenses >= 0) ?  (formattedSum + " to spend") : (formattedSum + " past goal");
 
     return (  
-        <div>
-            <p>{category.name}</p>
-            <p>{category.goal}</p>
+        <div className="category-container">
+            <div className="category-container-title">
+                <h1>{category.name}</h1>
+                <button className="category-buttons">See Detailed View</button>
+            </div>
+            <div className="category-container-info">
+                <h3 className="category-to-spend">{totalToSpend}</h3>
+                <p>${category.sumOfExpenses} / ${category.goal}</p>
+                <progress max={category.goal} value={category.sumOfExpenses} className={("category-progress-bar ") + ((sum > 0) ? "meeting-goal" : (sum === 0) ? "at-goal" : "past-goal")}/>
+            </div>
         </div>
     );
 }
