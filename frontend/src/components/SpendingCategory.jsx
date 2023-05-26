bfe4e720b9d2eeddf596c19e20eb87433b61174a
Jackson-Wozniak <jjw30@comcast.net>
2023-05-26 16:09:14 -0400

    style window UI

--- frontend/src/components/SpendingCategory.jsx
+++ frontend/src/components/SpendingCategory.jsx
@@ -12,15 +12,17 @@ function SpendingCategory(props) {
     let totalToSpend = (category.goal - category.sumOfExpenses >= 0) ?  (formatter.format(sum) + " to spend") : (formatter.format(sum * -1) + " past goal");
 
     return (  
-        <div className="category-container">
+        <div className="category-list-container">
             <div className="category-container-title">
                 <h1>{category.name}</h1>
-                <button className="category-buttons">See Detailed View</button>
+                <button className="inner-category-buttons">See Detailed View</button>
+                <h3 className="category-to-spend">{totalToSpend}</h3>
             </div>
             <div className="category-container-info">
-                <h3 className="category-to-spend">{totalToSpend}</h3>
-                <p>${category.sumOfExpenses} / ${category.goal}</p>
-                <progress max={category.goal} value={category.sumOfExpenses} className={("category-progress-bar ") + ((sum > 0) ? "meeting-goal" : (sum === 0) ? "at-goal" : "past-goal")}/>
+                <div className="progress-bar-container">
+                    <progress max={category.goal} value={category.sumOfExpenses} className={("category-progress-bar ") + ((sum > 0) ? "meeting-goal" : (sum === 0) ? "at-goal" : "past-goal")}/>
+                    <span>${category.sumOfExpenses} / ${category.goal}</span>
+                </div>
             </div>
         </div>
     );
